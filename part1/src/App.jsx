b55c48e7d4b70ef1c541d084966b0477d21da386
William Lane <wlane@cloudflare.com>
2026-01-27 20:37:00 -0600

    use arrays

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,40 +4,39 @@ const Header = (props) => (
 
 const Content = (props) => (
   <div>
-    <Part part={props.part1} exercises={props.exercises1} />
-    <Part part={props.part2} exercises={props.exercises2} />
-    <Part part={props.part3} exercises={props.exercises3} />
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
   </div>
 )
 
 const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.part.name} {props.part.exercises}
   </p>
 )
 
 const Summary = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
 
 const App = (props) => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 200
+  const course = "Half Stack application development"
+  const parts = [
+    {"name": "Fundamentals of React", "exercises": 10},
+    {"name": "Using props to pass data", "exercises": 7},
+    {"name": "State of a component", "exercises": 200},
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
-      <Summary exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /> 
+      <Content parts={parts} />
+      <Summary parts={parts} /> 
     </div>
   )
 }
