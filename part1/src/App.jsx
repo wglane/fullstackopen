8aab0b4ec58cc8e56fa53f5814fcf706dd03df18
William Lane <wlane@cloudflare.com>
2026-01-28 16:33:33 -0600

    refactor course vars into Object in App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -25,18 +25,21 @@ const Summary = (props) => {
 }
 
 const App = (props) => {
-  const course = "Half Stack application development"
-  const parts = [
-    {"name": "Fundamentals of React", "exercises": 10},
-    {"name": "Using props to pass data", "exercises": 7},
-    {"name": "State of a component", "exercises": 200},
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {"name": "Fundamentals of React", "exercises": 10},
+      {"name": "Using props to pass data", "exercises": 7},
+      {"name": "State of a component", "exercises": 14},
+    ]
+  } 
+
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Summary parts={parts} /> 
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Summary parts={course.parts} /> 
     </div>
   )
 }
